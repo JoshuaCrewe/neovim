ff2c91bf74f799cbfa1a2b20531a796a9caf6bab
Joshua <joshua@venncreative.co.uk>
2022-07-06 16:46:07 +0100

    Base pass at seoul 256 colours

--- lua/rose-pine/palette.lua
+++ lua/rose-pine/palette.lua
@@ -1,17 +1,19 @@
 local variants = {
 	main = {
-		base = '#191724',
-		surface = '#1f1d2e',
+		base = '#1c1c1c',
+		surface = '#111111',
 		overlay = '#26233a',
 		muted = '#6e6a86',
 		subtle = '#908caa',
-		text = '#e0def4',
-		love = '#eb6f92',
-		gold = '#f6c177',
-		rose = '#ebbcba',
-		pine = '#31748f',
-		foam = '#9ccfd8',
-		iris = '#c4a7e7',
+		text = '#d0d0d0',
+
+		love = '#d68787',
+		gold = '#d7d7af',
+		rose = '#d7afaf',
+		pine = '#5f865f',
+		foam = '#85add4',
+		iris = '#ffafaf',
+
 		highlight_low = '#21202e',
 		highlight_med = '#403d52',
 		highlight_high = '#524f67',
